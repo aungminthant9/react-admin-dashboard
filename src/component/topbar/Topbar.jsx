12392eb5c8a4b039bd55d40253af1486ba41296e
aungminthantX <johnsonherryson222@gmail.com>
2024-01-07 21:03:27 +0630

    first commit

---
+++ src/component/topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+const Topbar = () => {
+    return (  
+        <div>
+            <div>
+                <div>left</div>
+                <div>right</div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Topbar;
