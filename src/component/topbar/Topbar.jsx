c768611bb239e1564463c329b9c463448779fd1d
aungminthantX <johnsonherryson222@gmail.com>
2024-01-07 21:46:45 +0630

    Second commit

--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -1,9 +1,16 @@
+import {NotificationsNone} from '@mui/icons-material';
 const Topbar = () => {
     return (  
-        <div>
-            <div>
-                <div>left</div>
-                <div>right</div>
+        <div className="w-full h-[50px] bg-white">
+            <div className="h-full py-0 px-[20px] flex items-center justify-between">
+                <div className='topLeft'>
+                    <span className="font-bold text-[30px] text-blue-800 cursor-pointer">Aungadmin</span>
+                </div>
+                <div className='topRight'>
+                    <div className=''>
+                      <NotificationsNone/>
+                    </div>
+                </div>
             </div>
         </div>
     );
