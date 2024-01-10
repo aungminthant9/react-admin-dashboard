486b4616c4881e7daa7f5fb596bd79bc0aa844d3
aungminthantX <johnsonherryson222@gmail.com>
2024-01-10 20:47:10 +0630

    //

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import Home from "./pages/Home";
 import Sidebar from "./component/sidebar/Sidebar";
 import Topbar from "./component/topbar/Topbar";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import UserList from "./pages/UserList";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
      <Topbar/>
      <div className="flex mt-[10px]">
        <Sidebar/>
-       <Home/>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/users" element={<UserList/>}/>
+       </Routes>
      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
